9dbf4cef7433d88ccbf875c388b61325f82d1aa5
CodeWithRaph <raphiki.gaming@gmail.com>
2025-01-19 00:10:45 +0100

    camera set

---
+++ app/(tabs)/CameraStyles.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraContainer: {
+    flex: 1,
+    width: '100%',
+    // marginBottom: 20,
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'black',
+    textAlign: 'center',
+    margin: 10,
+  },
+  message: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import DualCameraView from '@/components/DualCameraView';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { styles } from './HomeScreenStyles';
 
 export default function HomeScreen() {
   return (
@@ -54,22 +54,3 @@ export default function HomeScreen() {
     </ParallaxScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
